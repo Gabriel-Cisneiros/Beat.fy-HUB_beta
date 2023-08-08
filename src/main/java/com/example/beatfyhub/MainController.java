d98cc6a3958d24af27392f9a9eef7f7f0b4cdba2
gabriel_cisneiros_ <gabrielcisneirossoliveira@gmail.com>
2023-08-08 01:03:19 -0300

    modificando o design original

--- src/main/java/com/example/beatfyhub/MainController.java
+++ src/main/java/com/example/beatfyhub/MainController.java
@@ -1,97 +1,35 @@
 package com.example.beatfyhub;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.layout.Pane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
+import javafx.scene.control.Button;
 
-import java.io.File;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class MainController implements Initializable{
+public class MainController {
 
     @FXML
-    private Pane mainBackgroundLabel;
+    private Button navbarButton1, navbarButton2, navbarButton3, navbarButton4;
+
     @FXML
-    private Label mediaNameLabel;
+    protected void initialize() {
+        navbarButton1.setOnAction(this::handleNavbarButton1Click);
+    }
     @FXML
-    private Button playPauseButton, previousButton, nextButton, shuffleButton, repeatButton;
+    private void handleNavbarButton1Click(ActionEvent event) {
+        System.out.println("Botão Navbar 1 clicado!");
+    }
+
     @FXML
-    private RadioButton likedButton;
+    private void handleNavbarButton2Click(ActionEvent event) {
+        System.out.println("Botão Navbar 2 clicado!");
+    }
+
     @FXML
-    private Slider mediaVolumeSlider;
+    private void handleNavbarButton3Click(ActionEvent event) {
+        System.out.println("Botão Navbar 3 clicado!");
+    }
+
     @FXML
-    private ProgressBar mediaProgressBar;
-
-    public Media media;
-    public MediaPlayer mediaPlayer;
-
-    private File directory;
-    private File[] files;
-
-    private ArrayList<File> songs;
-
-    private int songNumber;
-
-    private Timer timer;
-    private TimerTask task;
-    private boolean running;
-
-    public MainController() {
+    private void handleNavbarButton4Click(ActionEvent event) {
+        System.out.println("Botão Navbar 4 clicado!");
     }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        songs = new ArrayList<File>();
-
-        directory = new File("music");
-
-        files = directory.listFiles();
-
-        if (files != null){
-
-            for (File file : files){
-                songs.add(file);
-            }
-        }
-
-//        media = new Media(songs.get(songNumber).toURI().toString());
-//        mediaPlayer = new MediaPlayer(media);
-//
-//        mediaNameLabel.setText(songs.get(songNumber).getName());
-
-    }
-
-    public void playPauseMedia(){
-
-    }
-    public void previousMedia(){
-
-    }
-    public void nextMedia(){
-
-    }
-    public void shuffleMedia(){
-
-    }
-    public void repeatMedia(){
-
-    }
-    public void likedMedia(){
-
-    }
-    public void beginTimer(){
-
-    }
-    public void stopTimer(){
-
-    }
-
 }
